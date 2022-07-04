03fbef08121e98097576ceac03cc31d893f662e5
Victoria Le <victoriale@Victorias-MacBook-Pro.local>
2022-07-04 11:23:28 -0400

    break down the prob

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write a function which takes two numbers and returns their sum
+// 1. Can I restate the problem in my own words?
+// 2. What are the inputs the go into the problem?
+// 3. What are the outputs that should come from the solution to the problem?
+// 4. Can the outputs be determined by the inputs?
+// 5. How should I label the important piece of data that are part of the prob?
