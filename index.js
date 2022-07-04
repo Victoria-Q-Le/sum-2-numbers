48f3ec8fe66e22bbfa0b572c0f672717bfb6ece7
Victoria Le <victoriale@Victorias-MacBook-Pro.local>
2022-07-04 11:24:26 -0400

    explore concrete examples

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 // Write a function which takes two numbers and returns their sum
 // 1. Can I restate the problem in my own words?
+// "implement addition"
+
 // 2. What are the inputs the go into the problem?
+// What if they leave off one number? What if the number is really long? is the number an integer? afloat?
+
 // 3. What are the outputs that should come from the solution to the problem?
+// If I put in the integer should I get an integer back? What about the float? Should I get the float back if the input was a float?
+
 // 4. Can the outputs be determined by the inputs?
+
 // 5. How should I label the important piece of data that are part of the prob?
+
+
+// Write a function which takes in a string and return counts of each character in the string.
+
+// charCount ("aaaa"): {a:4}
+// charCount("hello"): {h:1, e:1, l:2, o:1}
+//
+// "my phone number is 1897536"?
